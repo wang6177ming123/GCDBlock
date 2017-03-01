a4c2247a39f672b53877d8bd8cdf50ae9d70b50b
wang6177ming123 <pactera@pactera2222222.local>
2017-03-01 20:41:14 +0800

    Initial Commit

---
+++ GCDBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDBlock
+//
+//  Created by pactera on 17/3/1.
+//  Copyright © 2017年 pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDBlock
+//
+//  Created by pactera on 17/3/1.
+//  Copyright © 2017年 pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
